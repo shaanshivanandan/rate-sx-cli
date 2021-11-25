718e200ff6126c63be12f6b0dbc6745cbf921c7b
Shaan Shivanandan <shaanshivanandan@protonmail.com>
2021-11-25 15:45:08 +0530

    Update rate.sh

--- rate.sh
+++ rate.sh
@@ -4,14 +4,10 @@ while [ $x -le 5 ]
 do
   curl rate.sx
   sleep 10
-  clear
   curl rate.sx/lrc
   sleep 10
-  clear
   curl rate.sx/sol
   sleep 10
-  clear
   curl rate.sx/eth
   sleep 10
-  clear
 done
